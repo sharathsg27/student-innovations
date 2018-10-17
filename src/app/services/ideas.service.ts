9befb877236ea2d6d063c9e3b0e4d87ab95c5dac
kishor <kishor.bl@adwitiya.io>
2018-10-17 11:36:17 +0530

    SL-3 List and Add Idea Components

---
+++ src/app/services/ideas.service.ts
@@ -0,0 +1,45 @@
+import {Injectable} from '@angular/core';
+import {AngularFireDatabase} from '@angular/fire/database';
+import {UUIDService} from '../utils/services/UUID/uuid.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class IdeasService {
+
+  constructor(private db: AngularFireDatabase,
+              private uuidService: UUIDService) {
+
+  }
+
+  /*list ideas*/
+  async getAllRecord(api: string) {
+    try {
+      return await this.db.database.ref(api).once('value')
+        .then(response => {
+          return response.val();
+        })
+        .catch((error: Error) => {
+          return error.message;
+        });
+    } catch (e) {
+      return e;
+    }
+  }
+
+  /*create idea*/
+  async createRecord(api: string, data: Object) {
+    try {
+      await this.db.database.ref(api + '/' + this.uuidService.generateUUID).set(data)
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((error: Error) => {
+          return error.message;
+        });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+}
