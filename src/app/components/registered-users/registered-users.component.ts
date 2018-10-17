085c56700051f9a56e0bf1bf1441415c2de1bbdb
sharath <sharath.s@adwitiya.io>
2018-10-17 11:49:38 +0530

    SL-1: Registered users screen added

---
+++ src/app/components/registered-users/registered-users.component.ts
@@ -0,0 +1,60 @@
+import {Component, OnInit} from '@angular/core';
+import {AppService} from '../../services/app.service';
+
+@Component({
+  selector: 'app-registered-users',
+  templateUrl: './registered-users.component.html',
+  styleUrls: ['./registered-users.component.css']
+})
+export class RegisteredUsersComponent implements OnInit {
+  users$: any[];
+  loading: boolean;
+
+  settings = {
+    columns: {
+      teacherName: {
+        title: 'Teacher',
+        editable: false
+      },
+      schoolName: {
+        title: 'School',
+        editable: false
+      },
+      schoolType: {
+        title: 'School Type',
+        editable: false
+      },
+      registerType: {
+        title: 'Register Type',
+        editable: false
+      },
+      city: {
+        title: 'City / District',
+        editable: false,
+      }
+    },
+    actions: {
+      add: false,
+      edit: false,
+      delete: false,
+    },
+    pager: {
+      perPage: 20
+    },
+    noDataMessage: 'No Registered records found'
+  };
+
+  constructor(private appService: AppService) {
+  }
+
+  ngOnInit() {
+    this.getRegisteredUsers();
+  }
+
+  async getRegisteredUsers() {
+    this.appService.loadingStatus.next(true);
+    this.users$ = Object.values(await this.appService.getAllRecord('/registration'));
+    this.appService.loadingStatus.next(false);
+  }
+
+}
