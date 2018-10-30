5e7a3bffe7f231a9e5ab20ebac4a5f5dd96218b2
sharath <sharath.s@adwitiya.io>
2018-10-30 20:23:08 +0530

    Master:
    Firebase api keys removed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,16 +4,9 @@
 
 export const environment = {
   production: false,
-  firebase: {
-    apiKey: 'AIzaSyAkpvE18xJJNdf-yvrKbh6Gd2BKpaAQ9zw',
-    authDomain: 'student-innovations.firebaseapp.com',
-    databaseURL: 'https://student-innovations.firebaseio.com',
-    projectId: 'student-innovations',
-    storageBucket: 'student-innovations.appspot.com',
-    messagingSenderId: '60699261995',
-  },
+  firebase: {},
   phoneSignInSettings: {
-    siteToken: '6Led8nMUAAAAAHRqRPF0xyg1EMTsFFeLDWOoms_g'
+    siteToken: ''
   },
   emailLinkAuthSettings: {
     url: 'https://localhost:3000/auth',
